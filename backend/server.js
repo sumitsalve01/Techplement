f0297f4f4704a8ffb5159521c36931a3793daa72
sumitsalve01 <139698732+sumitsalve01@users.noreply.github.com>
2024-06-08 21:54:23 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ app.use(cors({
 
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname,'/frontend/build')));
+app.use(express.static(path.join(__dirname,'frontend/build')));
 app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname,'/frontend/build/index.html'));
+    res.sendFile(path.join(__dirname,'frontend','build','index.html'));
 });
 
 
